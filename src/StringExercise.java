0ff5cc6c0e8587e4879de119006ca517bc15f26d
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-29 11:06:00 -0600

    -

---
+++ src/StringExercise.java
@@ -0,0 +1,18 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        // TODO For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+
+        String pinkFloyd = "We don't need no education.\nWe don't need no thought control.";
+        System.out.println((pinkFloyd));
+//
+        String checkOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println((checkOut));
+//
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println((windows));
+//
+//        String backSlashes = "I can do backslashes \, double backslashes \\, and the amazing triple backslash \\\!";
+//        System.out.println((backSlashes));
+    }
+}
