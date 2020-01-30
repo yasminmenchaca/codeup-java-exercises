5c6c333e198077cef845c46efe86364d58e25503
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-30 12:35:50 -0600

    created start buttons and title

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,11 @@ public class Game {
 
     JFrame window;
     Container con;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 80);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton;
 
     public static void main(String[] args) {
 
@@ -18,7 +23,30 @@ public class Game {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().setBackground(Color.black);
         window.setLayout(null);
-        window.setVisible(true);
+//        window.setVisible(true);
         con = window.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 600, 150);
+        titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("Adventure Game");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300,400,200,100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("Start");
+//        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.black);
+        startButton.setFont(normalFont);
+
+        titleNamePanel.add(titleNameLabel);
+        startButtonPanel.add(startButton);
+        con.add(titleNamePanel);
+        con.add(startButtonPanel);
+
+        window.setVisible(true);
     }
 }
