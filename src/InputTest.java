381fbe2691e44a9d332693df285940be864a3c21
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-31 13:57:48 -0600

    completed prior to walk through

---
+++ src/InputTest.java
@@ -0,0 +1,15 @@
+import util.Input;
+
+public class InputTest {
+    public static void main(String[] args) {
+
+        Input runInput = new Input();
+
+        System.out.println(runInput.getString());
+        System.out.println(runInput.yesNo());
+        System.out.println(runInput.getInt(1, 10));
+        System.out.println(runInput.getInt());
+        System.out.println(runInput.getDouble(1, 10));
+        System.out.println(runInput.getDouble());
+    }
+}
---
+++ src/util/Input.java
@@ -0,0 +1,56 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        System.out.println("Type anything: ");
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("Yes/No?");
+        String yesOrNo = scanner.nextLine();
+        return yesOrNo.equalsIgnoreCase("yes") || yesOrNo.equalsIgnoreCase("y");
+    }
+
+    public int getInt(int min, int max) {
+        System.out.format("Choose number between %s and %s: \n", min, max);
+        int userInput = Integer.parseInt(scanner.nextLine());
+        if (userInput <= max && userInput >= min) {
+            return userInput;
+        } else {
+            System.out.format("Invalid. Choose number between %s and %s: \n", min, max);
+            return getInt(min, max);
+        }
+
+    }
+
+    public int getInt() {
+        System.out.println("Enter any number: ");
+        return scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max) {
+
+        System.out.format("Choose number between %s and %s: \n", min, max);
+        double userDouble = scanner.nextDouble();
+        if (userDouble >= min && userDouble <= max) {
+            return userDouble;
+        } else {
+            System.out.format("Invalid. Choose number between %s and %s: \n", min, max);
+            return getDouble(min, max);
+        }
+    }
+
+    public double getDouble() {
+        System.out.println("Enter any number: ");
+        return scanner.nextDouble();
+    }
+}
