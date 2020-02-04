332c5352a9f70c369fa603c527ee242767d31ac4
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-04 15:24:06 -0600

    completed first exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,21 +5,21 @@ public class ShapesTest {
 
         // TODO create a variable of the type Rectangle named box1 and assign it a new instance of the Rectangle class with a width of 4 and a length of 5
 
-        Rectangle box1 = new Rectangle(5, 4);
+//        Rectangle box1 = new Rectangle(5, 4);
 
         //TODO verify that the getPerimeter and getArea methods return 18 and 20, respectively.
 
-        System.out.println(box1.getArea()); // 20
-        System.out.println(box1.getPerimeter()); // 18
+//        System.out.println(box1.getArea()); // 20
+//        System.out.println(box1.getPerimeter()); // 18
 
         //TODO create a variable of the type Rectangle named box2 and assign it a new instance of the Square class that has a side value of 5.
 
-        Rectangle box2 = new Square(5);
+//        Rectangle box2 = new Square(5);
 
         //TODO verify that the getPerimeter and getArea methods return 20 and 25, respectively.
 
-        System.out.println(box2.getArea()); // 25
-        System.out.println(box2.getPerimeter());// 20
+//        System.out.println(box2.getArea()); // 25
+//        System.out.println(box2.getPerimeter());// 20
 
     }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -26,20 +26,20 @@ public class Square extends Rectangle {
 //    ------------------- Code from prev exercise -------------------
         // TODO Square should define a constructor that accepts one argument, side, and calls the parent's constructor to set both the length and width to the value of side
 
-
     //  Whenever a parent and child class have same named methods then to resolve ambiguity we use super keyword.
-    public Square(int side) {
-        super(side, side);
-    }
+
+//    public Square(int side) {
+//        super(side, side);
+//    }
 
     // TODO override the getArea and getPerimeter methods
 
-    public int getPerimeter() {
-        return 4 * this.length;
-    }
+//    public int getPerimeter() {
+//        return 4 * this.length;
+//    }
 
-    public int getArea() {
-        return (int) Math.pow(this.length, 2);
-    }
+//    public int getArea() {
+//        return (int) Math.pow(this.length, 2);
+//    }
 
 }
