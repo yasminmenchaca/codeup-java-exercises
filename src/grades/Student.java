e82d268ab901c75f6105e862903f3ca989ec7e44
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-05 16:57:42 -0600

    in progress

---
+++ src/grades/Student.java
@@ -0,0 +1,29 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String names;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, int grade) {
+        this.names = name;
+        this.grades = grade;
+    }
+
+    // returns the student's name
+    public String getName() {
+        return null;
+    }
+
+    // adds the given grade to the grades property
+    public void addGrade(int grade) {
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage() {
+        return 0;
+    }
+
+}
