0985e6127df858faa26bfa5565de7210d3406462
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-05 21:19:07 -0600

    in progress

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,28 +1,46 @@
 package grades;
 
+import util.Input;
+
 import java.util.HashMap;
+import java.util.Scanner;
 
 // TODO Inside the main method, create a HashMap named students. It should have keys that are strings that represent github students, and values that are Student objects. Create at least 4 student objects with at least 3 grades each, and add them to the map.
 
 public class GradesApplication {
     public static void main(String[] args) {
+        HashMap<String, Student> students = new HashMap<>();
 
-        HashMap<String, String> students = new HashMap<>();
-        students.put("Ryan", "ryanorsinger");
+        Scanner scanner = new Scanner(System.in);
+        Input input = new Input();
+        boolean userAnswer;
 
-        System.out.println(students); // {Ryan=ryanorsinger}
+        Student larry = new Student("Larry");
+        larry.addGrade(77);
+        larry.addGrade(93);
+        larry.addGrade(89);
+        students.put("gordo_91", larry);
 
-        students.putIfAbsent("Zach", "zgulde");
-        students.putIfAbsent("Zach", "coderdude24");
-        System.out.println(students); // {Ryan=ryanorsinger, Zach=zgulde}
+        Student adam = new Student("Adam");
+        adam.addGrade(75);
+        adam.addGrade(91);
+        adam.addGrade(71);
+        students.put("adam.sanchez", adam);
 
-        students.remove("Zach");
-        System.out.println(students); // {Ryan=ryanorsinger}
+        Student yasmin = new Student("Yasmin");
+        yasmin.addGrade(80);
+        yasmin.addGrade(74);
+        yasmin.addGrade(80);
+        students.put("yasminismean", yasmin);
 
-        students.replace("Ryan", "torvalds");
-        System.out.println(students); // {Ryan=torvalds}
+        Student ismael = new Student("Ismael");
+        ismael.addGrade(91);
+        ismael.addGrade(74);
+        ismael.addGrade(81);
+        students.put("chemical_blaze", ismael);
 
-        students.clear();
-        students.isEmpty(); // true
+        // TODO Create the command line interface
+
+
     }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -8,9 +8,10 @@ public class Student {
     private ArrayList<Integer> grades;
 
     // TODO The student class should have a constructor that sets name property, and initializes the grades property as an empty ArrayList.
-    public Student(String name, ArrayList<Integer> grades) {
+
+    public Student(String name) {
         this.name = name;
-        this.grades = grades;
+        this.grades = new ArrayList<>();
     }
 
     // returns the student's name
@@ -28,22 +29,25 @@ public class Student {
     // returns the average of the students grades
     public double getGradeAverage() {
         double sum = 0;
-        for (int i = 0; i < grades.size(); i++) {
-            sum += grades.get(i);
+        for (Integer grade : grades) {
+            sum += grade;
         }
         return sum / grades.size();
     }
 
     // TODO Test your Student class by adding a main method and creating Student objects. Verify that you can add grades to each object, and that your getGradeAverage method correctly returns the average of the student's grades.
     public static void main(String[] args) {
-        ArrayList<Integer> studentGrades = new ArrayList<>();
-        studentGrades.add(81);
-        studentGrades.add(95);
-        studentGrades.add(73);
-        Student student = new Student("Yasmin", studentGrades);
-        System.out.format("Student: %s", student.getName());
-        System.out.format("\nAverage: %s", student.getGradeAverage());
-        student.addGrade(78);
-        System.out.format("\nNew Average is %s", student.getGradeAverage());
+        Student lynn = new Student("Lynn");
+        lynn.addGrade(81);
+        lynn.addGrade(95);
+        lynn.addGrade(73);
+        System.out.println("Student Name: " + lynn.getName());
+        System.out.println("Student Avg: " + lynn.getGradeAverage());
+        lynn.addGrade(100);
+        System.out.println("New Avg: " + lynn.getGradeAverage());
     }
+
+//    public ArrayList<Integer> getGrades(){
+//        return this.grades;
+//    }
 }
