a775bee9b4c022e3295f2fe01d54e14511dee821
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-13 10:56:21 -0600

    made correction

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -11,7 +11,7 @@ public class ArraysExercises {
         // TODO Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element. Iterate through the array and print out the name of each person in the array.
         //  Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
 
-//         intializing an array arr1
+//         initializing an array arr1
         String[] arr1 = {"Ismael", "Michael", "Adam"};
 
 //         printing the array
