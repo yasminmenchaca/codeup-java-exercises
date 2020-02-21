89437e15ce42618869b6732332fcf315e1edb04b
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-02-21 09:50:31 -0600

    completed warmup

---
+++ src/CodeupUtils.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class CodeupUtils {
+    public static void main(String[] args) {
+
+    }
+
+    // this is a method that takes in a list of integers and an int.
+    // it parses the list to find which pairs of integers in the list when added together equals the int parameter.
+    // return the result as a list of lists of integers.
+
+    /**
+     * @param listToTry a list to try to sum
+     * @param target    a target to test for suming
+     * @return the list of sums -  ArrayList<ArrayList<Integer>>
+     */
+
+    public static ArrayList<ArrayList<Integer>> findAddends(ArrayList<Integer> listToTry, int target) {
+        ArrayList<ArrayList<Integer>> sums = new ArrayList<>();
+
+        return sums;
+    }
+
+}
